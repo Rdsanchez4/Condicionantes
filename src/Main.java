553a903b28c5d820a47552a2d58e1a817b463427
Rdsanchez4 <yoshysanchez4@gmail.com>
2022-06-28 10:41:45 -0400

    entrega fundamentos

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args){
+
+        int NumeroFl=(-9);
+
+        if (NumeroFl > 0){
+        System.out.println("es positivo");
+    } else if (NumeroFl < 0) {
+        System.out.println("es negativo");
+    }
+
+      int NumeroWhile= (3);
+        while (NumeroWhile >3){
+            System.out.println("NumeroWhile");
+            NumeroWhile=NumeroWhile=(3);
+        }
+
+        int numeroWhile=(5);
+
+    do {
+            System.out.println(numeroWhile);
+            numeroWhile=numeroWhile -1;
+    } while (numeroWhile>10);
+        {
+    }
+                } }
